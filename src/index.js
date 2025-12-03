c381c624267a772e9ba8c3336db25879615eb1b0
sobuj <abdulaualsobuj@gmail.com>
2025-12-03 12:21:46 +0600

    project end

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { getMovieReviewData } from "./data.js";
-console.log(getMovieReviewData());
+// console.log(getMovieReviewData());
+
+let sortDes = false;
 
 function init() {
   const movieReviewData = getMovieReviewData();
+  registerHandlers(movieReviewData);
   paintStatistics(movieReviewData);
   paintMovieData(movieReviewData);
 }
+
 function paintStatistics(movieReviewData) {
   const flatReviewData = movieReviewData.flat();
   const totalMovies = movieReviewData.length;
@@ -13,6 +17,7 @@ function paintStatistics(movieReviewData) {
   const totalRating = flatReviewData.reduce((acc, item) => {
     return acc + item.rating;
   }, 0);
+
   const avrageRating = (totalRating / totalReviews).toFixed(2);
 
   const totalmoviesElement = document.getElementById("totalMovie");
@@ -34,14 +39,74 @@ function paintStatistics(movieReviewData) {
 
 function paintMovieData(movieReviewData) {
   const flatReviewData = movieReviewData.flat();
-  const movieListElement = document.querySelector("#Movielist UL");
-  console.log(movieListElement);
+  const sorted = flatReviewData.toSorted((a, b) => b.on - a.on);
+  const movieListElement = document.querySelector("#movielist UL");
 
-  flatReviewData.map((movie) => {
-    console.log(movie);
+  addMovieReviewData(movieListElement, sorted);
+}
+
+function registerHandlers(movieReviewData) {
+  const sortButton = document.getElementById("sortBtn");
+  const groupButton = document.getElementById("groupBtn");
+
+  sortButton.addEventListener("click", () => sortByReview(movieReviewData));
+  groupButton.addEventListener("click", () => groupReviewByTitle(movieReviewData)
+  );
+}
+function sortByReview(movieReviewData) {
+  // console.log("sortByReview");
+  sortDes = !sortDes;
+  const flatReviewData = movieReviewData.flat();
+
+  let sortReviewData = sortDes
+    ? flatReviewData.toSorted((a, b) => b.rating - a.rating)
+    : flatReviewData.toSorted((a, b) => a.rating - b.rating);
+
+  const movieListElement = document.querySelector("#movielist UL");
+  removeAllChildNodes(movieListElement);
+
+  addMovieReviewData(movieListElement, sortReviewData);
+}
+function groupReviewByTitle(movieReviewData) {
+  // console.log("groupReviewByTitle", movieReviewData)
+  const flatReviewData = movieReviewData.flat();
+  const groupedRivews = Object.groupBy(flatReviewData, ({ title }) => title);
+  const titlekeys = Reflect.ownKeys(groupedRivews);
+
+  const movieListElement = document.querySelector("#movielist UL");
+
+  removeAllChildNodes(movieListElement);
+
+  titlekeys.forEach((title) => {
+    const liElement = document.createElement("li");
+    liElement.classList.add("card", "mx-3");
+    const hElement = document.createElement("h2");
+    hElement.classList.add("text-3xl");
+    hElement.innerText = title;
+    liElement.appendChild(hElement);
+
+    const reviews = groupedRivews[title];
+    reviews.forEach((review) => {
+      const peraElement =document.createElement("p");
+      peraElement.classList =("mx-2", "my-2");
+      
+      const message  =`👍<strong>${review.by}</strong> has given <strong>${review.rating}</strong> rating with a comment <i>${review.content}</i>`;
+      peraElement.innerHTML = message;
+      liElement.appendChild(peraElement);
+    })
+
+    movieListElement.appendChild(liElement);
+  });
+} 
+
+function addMovieReviewData(movieListElement, movieRevie) {
+  // console.log(movieListElement);
+
+  movieRevie.map((movie) => {
+    // console.log(movie);
 
     const liElement = document.createElement("li");
-    liElement.classList.add("card", "p-2", );
+    liElement.classList.add("card", "p-2");
 
     const titleElement = document.createElement("p");
     titleElement.classList.add("text-xl", "mb-2");
@@ -64,4 +129,10 @@ function paintMovieData(movieReviewData) {
     movieListElement.appendChild(liElement);
   });
 }
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
 init();
